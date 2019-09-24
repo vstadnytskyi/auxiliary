ccb33cded5f4ec65f2e3dcfaff5fab51a6f683da
v.stadnytskyi@gmail.com <v.stadnytskyi@gmail.com>
2019-09-23 22:45:43 -0400

    added threading library

--- ubcs_auxiliary/threading.py
+++ ubcs_auxiliary/threading.py
@@ -26,7 +26,7 @@ def start_new_thread(function, *args , **kwargs):
             t2 = time()
             print('this thread slept for {} seconds'.format(t2-t1))
             print('input parameters: args = {}, kwargs = {!r}'.format(arg[0],kwargs['keyword']))
-    >>> start_new_thread(func,N, keyword = M)
+    >>> start_new_thread(func, 1, keyword = 'this is key 1 value')
     this thread slept for 1.002244234085083 seconds
     input parameters: args = 1, kwargs = 'keywords argument'
     """
@@ -43,4 +43,4 @@ def test_start_new_thread(N = 3,M = 'this is key 1 value'):
         t2 = time()
         print('this thread slept for {} seconds'.format(t2-t1))
         print('input parameters: args = {}, kwargs = {!r}'.format(arg[0],kwargs['keyword']))
-    start_new_thread(func,N, keyword = M)
+    start_new_thread(func,N , keyword = M)
