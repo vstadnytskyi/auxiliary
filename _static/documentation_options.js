bcb0c38c10d25dad0edf67b83e63db2caf7f73d3
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-12-04 23:11:41 +0000

    Update docs after building Travis build 112 of vstadnytskyi/auxiliary
    The docs were built from the branch 'master' against the commit
    dea52a13e9b89b7b53c1198fd1c4b24042910e3e.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/vstadnytskyi/auxiliary/jobs/620870845.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post50+g3f6399e',
+    VERSION: '0.post50+gdea52a1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
