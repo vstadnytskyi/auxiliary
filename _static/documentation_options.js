6c13fa1b332498d9bf7c5f74fc1d471e13c1667e
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-06-23 21:34:04 +0000

    Update docs after building Travis build 130 of vstadnytskyi/auxiliary
    The docs were built from the branch 'master' against the commit
    e32abf92a47c8d805f7549bacf77778c5a742fd3.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/vstadnytskyi/auxiliary/jobs/701433544.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,10 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.19.post5+g859f404',
+    VERSION: '0.0.19.post27+ge32abf9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
--- examples/numerical_bin_data.py
+++ examples/numerical_bin_data.py
@@ -9,6 +9,6 @@ binned_data = bin_data(data  = data, x = x, num_of_bins = 100, dtype = 'float')
 import matplotlib.pyplot as plt
 fig, [ax1, ax2] = plt.subplots(nrows=2, ncols=1)
 ax1.plot(x,data[:,0],'-')
-ax2.fill_between(binned_data['x'], binned_data['y_min'][0], binned_data['y_max'][0], color='grey', alpha='0.5')
+ax2.fill_between(binned_data['x'], binned_data['y_min'][0], binned_data['y_max'][0], color='grey', alpha=0.5)
 ax2.plot(binned_data['x'],binned_data['y_mean'][0],'-')
 fig.show()
